96293ee263d8ca0d2c692db9359fc83e0c293049
Onesuch07 <onesuch07@gmail.com>
2017-08-17 13:33:41 +0300

    Закночил верстку

--- content/content.js
+++ content/content.js
@@ -2,7 +2,7 @@ var logic = false;
 
 $(document).ready(function () {
     var url = 'https://oauth.vk.com/authorize?client_id=5894587&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&messages&photos&audio&video&status&wall&docs&groups&notifications&stats&email&response_type=token&v=5.52',
-        tokken = '85ac061110884098a26535294c604b7ba3b63b7746140e760f3842556e5226553935df4bfcbd7330e7f1a'; // Полученный токен
+        tokken = '3a2098e4d9baeee7de57528480b312ddad8d5546193e156d6de28dfb80a168ad3f94cdf410b578dc78ee4'; // Полученный токен
 
     localStorage.setItem('Tokken', tokken); // записали токен в localstorage
 
@@ -11,12 +11,19 @@ $(document).ready(function () {
     /** Получаем данные для Контента **/
     $.ajax({
         maethod: 'POST',
-        url: 'https://api.vk.com/method/users.get?v=5.52&access_token=' + storageTokken,
+        url: 'https://api.vk.com/method/users.get?fields&v=5.52&access_token=' + storageTokken,
         dataType : "jsonp"
     }).done(function (data) {
-        var userNameLastName = document.getElementsByClassName('user-name')[1];
 
-        userNameLastName.innerHTML = data.response[0].first_name + ' '+ data.response[0].last_name;
+        console.log('data', data);
+
+        var userNameLastName = document.getElementsByClassName('last-name-name'),
+            dataUser = document.getElementsByClassName('data-user');
+
+        for (var i = 0; i < userNameLastName.length; i++) {
+            userNameLastName[i].innerHTML = data.response[0].first_name + ' '+ data.response[0].last_name;
+            dataUser.innerHTML = data.response[0].data;
+        }
 
     });
 
--- header/header.js
+++ header/header.js
@@ -1,12 +1,5 @@
 $(document).ready(function () {
-    var url = 'https://oauth.vk.com/authorize?client_id=5894587&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52',
-        tokken = 'c36d83e6754367880db7c4e52ebd7b461cc28c4e54c7c0ac0ce5493b42a6dfcf089ccaedb4995e5dabf7f'; // Полученный токен
-
-    localStorage.setItem('Tokken', tokken); // записали токен в localstorage
-
     var storageTokken =  localStorage.getItem('Tokken'); // берём по ключу
-
-    // localStorage.removeItem('Tokken') // Удалить ил Localstorage
 
     /** Получаем данные в Header **/
     $.ajax({
